872bbb4dd6cc58d349bfe4ff0d884d84e7622494
amanraj953426 <amanraj953426@gmail.com>
2025-09-01 22:30:54 +0530

    change comment

--- Multithreading/createThread/main.java
+++ Multithreading/createThread/main.java
@@ -7,7 +7,7 @@ public class main {
 //        thread.start();// start method to start thread
 
         Runnable_interface runnable = new Runnable_interface(); // If we Implement Runnable, first create object
-        Thread t1 = new Thread(runnable); // after that we have to create Thread object and pass runnable interface
+        Thread t1 = new Thread(runnable);   // after that we have to create Thread object and pass runnable interface
                                             // in this constructor
         
         t1.start(); // then we stat thread
